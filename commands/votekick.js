62ea698e607099c8c2af25d247230338b9e1f182
jacobrhubbard <jacobrhubbard@gmail.com>
2022-01-01 22:06:55 -0500

    Salazan now able to kick, working on permissions now

--- commands/votekick.js
+++ commands/votekick.js
@@ -2,16 +2,17 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('votekick')
+		.setName('kill')
 		.setDescription('Remove from the realm')
         .addUserOption(option => 
 			option
 				.setName('target')
 				.setRequired(true)
-				.setDescription('voting to ban')
+				.setDescription('the target')
 			),
 	async execute(interaction) {
 		const target = interaction.options.getMember('target');
-		await interaction.reply({ content:`BANNING ${target}` });
+		await interaction.reply({ content:`SMITING ${target}` });
+		target.kick("Salazan has smited you :)");
 	},
 };
